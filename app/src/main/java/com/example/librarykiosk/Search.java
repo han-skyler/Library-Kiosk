418dd31809efb9935f9411874bf561ec6aed617c
skyler <april260@naver.com>
2024-01-22 19:33:15 +0900

    ver 0.0.1

--- app/src/main/java/com/example/librarykiosk/Search.java
+++ app/src/main/java/com/example/librarykiosk/Search.java
@@ -3,12 +3,22 @@ package com.example.librarykiosk;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Search extends AppCompatActivity {
+
+    private TextView mBtnSearch;
+    private EditText mEtSerch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        mBtnSearch = findViewById(R.id.btn_search);
+        mEtSerch = findViewById(R.id.et_search);
+
+
     }
 }
